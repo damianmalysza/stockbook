7ebc2442009ea85d285386738ae5766670f09938
Damian Malysza <damian.malysza@gmail.com>
2021-02-24 22:09:58 -0500

    fixed activerecord version issues

--- db/migrate/20210224233433_create_stock_portfolios.rb
+++ db/migrate/20210224233433_create_stock_portfolios.rb
@@ -1,4 +1,4 @@
-class CreateStockPortfolios < ActiveRecord::Migration
+class CreateStockPortfolios < ActiveRecord::Migration[5.2]
   def change
     create_table :stock_portfolios do |t|
       t.integer :stock_id
