3da73310f5dd210100cb7ca6036fb20f8695e84e
Damian Malysza <damian.malysza@gmail.com>
2021-03-01 18:43:53 -0500

    added logic to signup route to redirect to home page if logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 require 'rack-flash'
 
 class UsersController < ApplicationController
-
+  
   get '/signup' do
-    erb :"users/signup"
+    if logged_in?
+      redirect '/'
+    else
+      erb :"users/signup"
+    end
   end
-
+  
   post '/signup' do
     if User.find_by(username: params[:username])
       flash[:message] = "Username already taken - please use another one"
@@ -16,41 +20,41 @@ class UsersController < ApplicationController
       redirect '/' #TODO - change this to render the portfolio view page for the user once that gets coded
     end
   end
-
+  
   get '/login' do
     erb :"users/login"
   end
-
+  
   post '/login' do
     
   end
-
+  
   get '/logout' do
     session.clear
     redirect '/'
   end
-
+  
   get "/users/:username" do
     erb :"/users/show.html"
   end
-
+  
   get "/users/:username/edit" do
     erb :"/users/edit.html"
   end
-
+  
   patch "/users/:username" do
     redirect "/users/:id"
   end
-
+  
   delete "/users/:username/delete" do
     redirect "/users"
   end
-
+  
   helpers do
     def current_user
       User.find_by(username:session[:username])
     end
-
+    
     def logged_in? 
       !!session[:username]
     end
